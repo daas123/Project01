6409c01ac1deaeeda7f5f97486a81f708bc96f75
Saad_Vadanagara <139956151+daas123@users.noreply.github.com>
2025-05-30 09:23:09 +0530

    initial_Commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const users = require("./MOCK_DATA.json");
+const app = express();
+const PORT = 8000;
+
+
+app.get('/users', (req,res) => {
+    const html = `
+    <ul>
+    ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+    </ul>
+    `;
+    res.send(html)
+});
+
+// routes
+app.get("/api/users", (req,res) => {
+    return res.json(users)
+});
+
+//creating new users
+app.post("/api/users",(req,res) => {
+    return res.json({ stauts : "pending"});
+});
+
+///dynamic path Parameter (:id)
+app.route('/api/users/:id')
+.get((req,res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+})
+.patch((req,res) => {
+    //edit User
+    return res.json({ status: "pending"});
+})
+.delete((req,res) => {
+    // delete user
+    return res.json({ status: "pending"});
+})
+
+
+app.listen(PORT,() => console.log(`server start at port ${PORT}`));
